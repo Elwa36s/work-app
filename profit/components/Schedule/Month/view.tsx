37a1b54e01d8fe9f0c95d87381c68a3b95354c65
Dzmitry Dauhapol <Dzmitry.Dauhapol@itechart-group.com>
2023-03-13 12:34:18 +0100

    add month click handler

--- profit/components/Schedule/Month/view.tsx
+++ profit/components/Schedule/Month/view.tsx
@@ -1,7 +1,9 @@
 import React, {FC, useCallback, useMemo} from 'react';
-import {getMonthWeeksStarts} from '@app/utils';
+import {useRouter} from 'next/router';
+import {getMonthWeeksStarts, formatDate} from '@app/utils';
 import {Container} from '@mui/material';
-import {format, startOfMonth, isSameMonth, addDays} from 'date-fns';
+import {format, startOfMonth, isSameMonth, addDays, isSameDay} from 'date-fns';
+import classNames from 'classnames';
 import styles from './styles.module.scss';
 
 interface MonthProps {
@@ -10,28 +12,54 @@ interface MonthProps {
 
 export const View: FC<MonthProps> = (props) => {
     const {dayOfMonth} = props;
+    const router = useRouter();
+
+    const dayClickHandler = useCallback((event: React.MouseEvent<HTMLDivElement>) => {
+        const target = event.target as HTMLDivElement;
+
+        if (target) {
+            const day = target.closest(`.${styles.month_day}`);
+            
+            if (day) {
+                const date = new Date(dayOfMonth);
+                date.setDate(parseInt(day.innerHTML));
+                router.push(`/day/${formatDate(date)}`);
+            }
+        }
+    }, [router, dayOfMonth]);
+
     const weeksDays = useMemo(() => {getMonthWeeksStarts(dayOfMonth)}, [dayOfMonth]);
     // const renderMonth = (weeksDays: Date[]) => weeksDays.map((day) => (<Week key={day.getMilliseconds()} date={day}/>));
     const month = format(dayOfMonth, 'LLLL');
     const legend = ['m', 't', 'w', 't', 'f', 's', 's'];
 
     const renderDays = useCallback(() => {
+        const today = new Date();
         const firstDay = startOfMonth(dayOfMonth);
         const firstDayOfWeek = firstDay.getDay();
         const daysArr: React.ReactNode[] = [];
 
         if (firstDayOfWeek !== 1) {
             const emptyLength = Math.abs(1 - firstDayOfWeek);
-            const emptyArr = new Array(emptyLength).fill(<div />);
-            daysArr.push(...emptyArr);
+            const emptyArr = new Array(emptyLength).fill('');
+            emptyArr.forEach((emptyCell, index) => {
+                daysArr.push(<div key={`empty-cell-${index}`}/>);
+            })
         };
 
         let renderedDay = new Date(firstDay);
         while (isSameMonth(firstDay, renderedDay)) {
             const date = renderedDay.getDate();
             const timestamp = renderedDay.getTime();
+            const isCurrentDay = isSameDay(today, renderedDay);
 
-            daysArr.push(<div key={timestamp} className={styles.month_day}>{date}</div>);
+            daysArr.push(
+                <div
+                    key={timestamp}
+                    className={classNames(styles.month_day, {[styles.current_day]: isCurrentDay})}
+                >
+                    {date}
+                </div>);
             renderedDay = addDays(renderedDay, 1);
         };
 
@@ -46,7 +74,7 @@ export const View: FC<MonthProps> = (props) => {
                     return <div key={index}>{day}</div>
                 })}
             </div>
-            <div className={styles.month_grid}>
+            <div className={styles.month_grid} onClick={dayClickHandler}>
                 {...renderDays()}
             </div>
         </Container>
